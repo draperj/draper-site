454672156bf4d370f61b44993d17cb006b646adb
Josh Draper <mrjacoste@gmail.com>
2018-03-25 14:45:46 -0400

    Testing of Heroku deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world Josh!</h1>
+          <h1 className="App-title">Hello world Josh! This is a test of our heroku deploy!</h1>
         </header>
       </div>
     );
